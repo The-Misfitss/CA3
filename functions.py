c0201ff892597933b7d35f6ebfc17ce9b4570e58
Muhammad Abdullah Cheema <notabdullah1025@gmail.com>
2023-09-19 11:14:15 +0500

    Added Divide Function in functions.py file and Test for division in tests.py file

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 
 def multiply(a, b):
     return a * b
-  
-  
+
+
 def add(x,y):
     return x+y
-  
-  
+
+
+def divide(x,y):
+    return x/y
--- tests.py
+++ tests.py
@@ -1,5 +1,10 @@
 from functions import add
+from functions import divide
 
 
 def test_add():
     assert 4 == add(2, 2)
+
+
+def test_divide():
+    assert 2 == divide(4, 2)
