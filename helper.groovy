6dd2b4af19c37e774bc6fef1211518f8dd12f8d6
The-Misfitss <145423645+The-Misfitss@users.noreply.github.com>
2023-10-22 13:23:28 +0500

    Create helper.groovy

---
+++ helper.groovy
@@ -0,0 +1,6 @@
+//Works as an object, at the end of the file this should be returned so that it can be used as an object in the jenkinsfile
+def deployit(){
+    echo "Deploying the application using the groovy Script..."
+}
+
+return this
